ef94fa3229b771cc6a9aa1d68d0e9181672cc105
Pedro Matos <109925892+pedrodabahia@users.noreply.github.com>
2023-11-27 15:07:58 -0300

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function(){
+
+    $('.mobile i').click(function(){    
+        var el =  $(this).parent().find('ul')
+
+        if(el.is(':visible') == false){
+            el.fadeIn();
+        }else{
+            el.fadeOut();
+        }
+
+    })
+
+
+})
